76333893764571bc379f70d62c63892535ac5282
AnuragShelar <2016.anurag.shelar@ves.ac.in>
2018-09-23 02:51:58 +0530

    navbar done

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+
+    articles=[
+    {
+        'id':1,
+        'title':'Article One',
+        'body':'Hello',
+        'author':'Anurag Shelar',
+        'create_date':'23-09-18'
+    },
+
+    {
+        'id':2,
+        'title':'Article Two',
+        'body':'Hello',
+        'author':'Anurag Shelar',
+        'create_date':'23-09-18'
+    },
+
+    {
+        'id':3,
+        'title':'Article Three',
+        'body':'Hello',
+        'author':'Anurag Shelar',
+        'create_date':'23-09-18'
+    },
+    ]
+
+    return articles
